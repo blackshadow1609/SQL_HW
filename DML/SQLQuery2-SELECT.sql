291d032f587724daaf4bc895d7ad0a17ff7d1e62
Sky_Time87 <blackshadow1609@yandex.ru>
2025-09-19 01:03:41 +0300

    Adding and counting students PD_321 DONE

--- DML/SQLQuery2-SELECT.sql
+++ DML/SQLQuery2-SELECT.sql
@@ -19,12 +19,38 @@ GO
 --AND		direction_name		LIKE(N'Сетевые технологии%')
 --;
 ------------------------------------------------------------------------------------------------------
-SELECT
-		last_name	+	''	+	first_name+	''	+	middle_name	AS	N'Студент',
-		group_name												AS	N'Группа',
-		direction_name											AS	N'Направление обучения'
-FROM	Students, Groups, Directions
-WHERE	[group]		=	group_id
-AND		direction	=	direction_id
-;
+--SELECT
+--		last_name	+	''	+	first_name+	''	+	middle_name	AS	N'Студент',
+--		group_name												AS	N'Группа',
+--		direction_name											AS	N'Направление обучения'
+--FROM	Students, Groups, Directions
+--WHERE	[group]		=	group_id
+--AND		direction	=	direction_id
+--;
 ------------------------------------------------------------------------------------------------------
+--SELECT
+--		COUNT(stud_id)	AS N'Количество студентов' 
+--FROM Students;
+------------------------------------------------------------------------------------------------------
+--------------------------------Количество студентов в каждой группе----------------------------------
+PRINT N'=== Количество студентов в каждой группе ==='
+SELECT
+    group_name AS N'Группа',
+    COUNT(stud_id) AS N'Количество студентов'
+FROM Students
+JOIN Groups ON [group] = group_id
+GROUP BY group_name
+ORDER BY group_name;
+
+PRINT N''
+
+-----------------------------Количество студентов на каждом направлении-------------------------------
+PRINT N'=== Количество студентов на каждом направлении ==='
+SELECT
+    direction_name AS N'Направление обучения',
+    COUNT(stud_id) AS N'Количество студентов'
+FROM Students
+JOIN Groups ON [group] = group_id
+JOIN Directions ON direction = direction_id
+GROUP BY direction_name
+ORDER BY direction_name;
